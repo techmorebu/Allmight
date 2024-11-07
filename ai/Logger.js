00d83991c55a0ec42abfdc0b895ab2214421b6bc
techmorebu <techmorebu@gmail.com>
2024-11-07 14:02:10 -0600

    Delete ai directory

--- ai/Logger.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function logTrade(action, details) {
-    const logEntry = `${new Date().toISOString()} - ${action}: ${JSON.stringify(details)}\n`;
-    fs.appendFileSync('trade_log.txt', logEntry, 'utf8');
-    console.log(logEntry);
-}
-
-module.exports = { logTrade };
