185f06b6dfda62aefaeac50a3b543dc5e7da77e8
techmorebu <techmorebu@gmail.com>
2024-11-07 14:30:41 -0600

    Delete src directory

--- src/trading/TradeEvaluator.js
+++
@@ -1,8 +0,0 @@
-const ethers5 = require("../aave-deps/node_modules/ethers");
-
-function evaluateTrade(price1, price2) {
-  const ratio = price1 / price2;
-  return ratio > 1.05;
-}
-
-module.exports = { evaluateTrade };
