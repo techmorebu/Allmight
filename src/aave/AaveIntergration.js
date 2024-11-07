8ba64aa337ed9b2ae3d74ed23f77a3fa8fb44779
techmorebu <techmorebu@gmail.com>
2024-11-07 14:33:24 -0600

    Create AaveIntergration.js

---
+++ src/aave/AaveIntergration.js
@@ -0,0 +1,11 @@
+// src/aave/AaveIntegration.js
+
+const ethers = require('../aave-deps/node_modules/ethers'); // Path to ethers@5
+const { AaveProtocol } = require('../aave-deps/node_modules/@aave/protocol-js');
+
+async function initAave() {
+    const provider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_MAINNET_RPC_URL_1);
+    // Aave logic goes here
+}
+
+module.exports = { initAave };
