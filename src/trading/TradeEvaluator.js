44f54ecedad4f7cdc0c26de4a709964c6398362e
techmorebu <techmorebu@gmail.com>
2024-11-07 13:30:05 -0600

    Update TradeEvaluator.js
    Includes npm link fix for ethers

--- src/trading/TradeEvaluator.js
+++ src/trading/TradeEvaluator.js
@@ -1,8 +1,8 @@
-const ethers = require("ethers");
+const ethers5 = require("../aave-deps/node_modules/ethers");
 
 function evaluateTrade(price1, price2) {
   const ratio = price1 / price2;
-  return ratio > 1.05;  // Example threshold
+  return ratio > 1.05;
 }
 
 module.exports = { evaluateTrade };
