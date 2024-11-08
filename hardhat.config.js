6dc76f07a2eeb745401910fa523ad4ae489386d8
techmorebu <techmorebu@gmail.com>
2024-11-07 18:53:59 -0600

    Initial OFA setup with Aave and Flashbots dependency fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
-require("dotenv").config();
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 
 module.exports = {
-    solidity: "0.8.17",
+    solidity: '0.8.9',
     networks: {
-        ethereum: {
-            url: process.env.ETHEREUM_MAINNET_RPC_URL_1,
+        mainnet: {
+            url: process.env.ETHEREUM_MAINNET_RPC_URL,
             accounts: [process.env.METAMASK_PRIVATE_KEY]
         },
         polygon: {
-            url: process.env.POLYGON_MAINNET_RPC_URL_1,
+            url: process.env.POLYGON_MAINNET_RPC_URL,
             accounts: [process.env.METAMASK_PRIVATE_KEY]
         },
-        // Add other networks as needed
+        // Add more networks as needed
     }
 };
