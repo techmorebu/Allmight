49152d9f52d46f8c4c98563278d0a07bc77fdd72
techmorebu <techmorebu@gmail.com>
2024-11-09 00:09:29 -0600

    Update checkEnv.js

--- checkEnv.js
+++ checkEnv.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 
-console.log("Ethereum Mainnet RPC URL:", process.env.ETHEREUM_MAINNET_RPC_URL);
-console.log("Polygon Mainnet RPC URL:", process.env.POLYGON_MAINNET_RPC_URL);
+console.log("Ethereum Mainnet RPC URL:", process.env.ETHEREUM_MAINNET_RPC_URL_1);
+console.log("Polygon Mainnet RPC URL:", process.env.POLYGON_MAINNET_RPC_URL_1);
