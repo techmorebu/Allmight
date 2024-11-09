92e79e60f83b1cf5e86f2c6c0d70849acefe064c
techmorebu <techmorebu@gmail.com>
2024-11-08 23:58:58 -0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,16 @@
-require("@nomicfoundation/hardhat-ethers");
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-    solidity: "0.8.17",
-    networks: {
-        ethereum: {
-            url: process.env.ETHEREUM_MAINNET_RPC_URL,
-            accounts: [process.env.METAMASK_PRIVATE_KEY]
-        },
-        polygon: {
-            url: process.env.POLYGON_MAINNET_RPC_URL,
-            accounts: [process.env.METAMASK_PRIVATE_KEY]
-        },
-        zksync: {
-            url: process.env.ZKSYNC_MAINNET_RPC_URL,
-            accounts: [process.env.METAMASK_PRIVATE_KEY]
-        },
-        scroll: {
-            url: process.env.SCROLL_MAINNET_RPC_URL_1,
-            accounts: [process.env.METAMASK_PRIVATE_KEY]
-        }
-    }
+  networks: {
+    ethereumMainnet: {
+      url: process.env.ETHEREUM_MAINNET_RPC_URL_1,
+      accounts: [process.env.METAMASK_PRIVATE_KEY]
+    },
+    polygonMainnet: {
+      url: process.env.POLYGON_MAINNET_RPC_URL_1,
+      accounts: [process.env.METAMASK_PRIVATE_KEY]
+    },
+    // Add configurations for each network as defined in the .env file
+  },
+  // Add Solidity compiler settings, etc.
 };
