864fb70e6899824d75077d5889a4b34bebf56f92
techmorebu <techmorebu@gmail.com>
2024-11-09 01:17:47 -0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-ethers");
+const { task } = require("hardhat/config");
 
 module.exports = {
-  solidity: "0.8.20",  // Using the latest stable version of Solidity
+  solidity: "0.8.20",
   networks: {
     mainnet: {
       url: process.env.ETHEREUM_MAINNET_RPC_URL_1,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
+    },
+    sepolia: {
+      url: process.env.ETHEREUM_TESTNET_SEPOLIA_RPC_URL,
       accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
     },
     polygon: {
@@ -16,6 +21,14 @@ module.exports = {
       url: process.env.ZKSYNC_MAINNET_RPC_URL,
       accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
     },
-    // Add other networks here following the same pattern
+    // Add other networks as necessary
   }
 };
+
+// Custom task to list accounts
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
