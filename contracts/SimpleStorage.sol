bf8170a40a5da521f7ca109332f3118a8c4564a7
techmorebu <techmorebu@gmail.com>
2024-11-20 21:54:33 -0600

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.8.20;
 
 contract SimpleStorage {
-  uint256 private data;
+    uint256 private data;
 
-  function set(uint256 _data) public {
-    data = _data;
-  }
+    // Event to log data updates
+    event DataUpdated(uint256 oldData, uint256 newData);
 
-  function get() public view returns (uint256) {
-    return data;
-  }
+    // Function to set data
+    function set(uint256 _data) public {
+        uint256 oldData = data;
+        data = _data;
+        emit DataUpdated(oldData, _data);
+    }
+
+    // Function to get data
+    function get() public view returns (uint256) {
+        return data;
+    }
 }
