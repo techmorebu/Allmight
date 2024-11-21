503a3c143f408698bd28a4a61e6886844f408bf8
techmorebu <techmorebu@gmail.com>
2024-11-20 20:56:20 -0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,24 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomicfoundation/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.20",
+  networks: {
+    mainnet: {
+      url: process.env.ETHEREUM_MAINNET_RPC_URL_1,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: process.env.ETHEREUM_TESTNET_SEPOLIA_RPC_URL,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
+    },
+    polygon: {
+      url: process.env.POLYGON_MAINNET_RPC_URL_1,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
+    },
+    zksync: {
+      url: process.env.ZKSYNC_MAINNET_RPC_URL,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
+    },
+  },
 };
