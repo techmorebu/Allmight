6de3c57861cffd055686ee441756b7e3c3f3c4e9
techmorebu <techmorebu@gmail.com>
2024-11-20 21:14:40 -0600

    Update deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
+  const simpleStorage = await SimpleStorage.deploy();
+  await simpleStorage.deployed();
+
+  console.log("SimpleStorage deployed to:", simpleStorage.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
