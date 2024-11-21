5fac88210a3a856b660fc4c8d866408ef2500df0
techmorebu <techmorebu@gmail.com>
2024-11-21 04:29:33 -0600

    Update fetchData.js

--- data-collection/fetchData.js
+++ data-collection/fetchData.js
@@ -4,12 +4,17 @@ const { logger } = require("../monitoring/logger");
 
 // Fetch token prices using CoinGecko API
 async function fetchTokenPrices() {
-    const url = `https://api.coingecko.com/api/v3/simple/price`;
+    const url = 'https://api.coingecko.com/api/v3/simple/price';
     const params = {
         ids: 'bitcoin,ethereum,polygon',
-        vs_currencies: 'usdt',
-        x_cg_pro_api_key: process.env.COINGECKO_API_KEY
+        vs_currencies: 'usd'
     };
+
+    // Include API key if using CoinGecko's Pro API
+    if (process.env.COINGECKO_API_KEY) {
+        params.x_cg_pro_api_key = process.env.COINGECKO_API_KEY;
+    }
+
     try {
         const response = await axios.get(url, { params });
         logger.info("Fetched token prices successfully.");
