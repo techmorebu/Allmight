b1a78d470e396fc71a6f44bb6a1c6688ab11d5aa
techmorebu <techmorebu@gmail.com>
2024-11-21 01:32:56 -0600

    Update interact.js

--- scripts/interact.js
+++ scripts/interact.js
@@ -4,7 +4,7 @@ const { ethers } = require("ethers");
 async function main() {
     const provider = new ethers.JsonRpcProvider(process.env.ETHEREUM_TESTNET_SEPOLIA_RPC_URL);
     const signer = new ethers.Wallet(process.env.METAMASK_PRIVATE_KEY, provider);
-    const contractAddress = "0xYourDeployedContractAddress";
+    const contractAddress = "0x52F56Eba61EC93F52990c210C62859332fa2a8B7";
     const abi = [
         "function set(uint256 x) public",
         "function get() public view returns (uint256)"
