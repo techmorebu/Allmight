e3aa450333dd260046752e6d36a02b848c91c1b4
techmorebu <techmorebu@gmail.com>
2024-11-20 21:33:18 -0600

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,22 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  // Get the contract factory
   const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
+
+  // Deploy the contract
   const simpleStorage = await SimpleStorage.deploy();
+
+  // Wait for deployment to be mined
   await simpleStorage.deployed();
 
-  console.log("SimpleStorage deployed to:", simpleStorage.address);
+  console.log("Contract deployed to:", simpleStorage.address);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Execute the deployment script
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
