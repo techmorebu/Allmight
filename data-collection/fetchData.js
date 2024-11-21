ea735423e0908c5e37d15552a1a151e8eefe11ab
techmorebu <techmorebu@gmail.com>
2024-11-21 04:24:02 -0600

    Update fetchData.js

--- data-collection/fetchData.js
+++ data-collection/fetchData.js
@@ -4,9 +4,14 @@ const { logger } = require("../monitoring/logger");
 
 // Fetch token prices using CoinGecko API
 async function fetchTokenPrices() {
-    const url = `https://api.coingecko.com/api/v3/simple/price?ids=ethereum,polygon,zksync&vs_currencies=usd&x_cg_pro_api_key=${process.env.COINGECKO_API_KEY}`;
+    const url = `https://api.coingecko.com/api/v3/simple/price`;
+    const params = {
+        ids: 'bitcoin,ethereum,polygon',
+        vs_currencies: 'usdt',
+        x_cg_pro_api_key: process.env.COINGECKO_API_KEY
+    };
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, { params });
         logger.info("Fetched token prices successfully.");
         return response.data;
     } catch (error) {
@@ -15,32 +20,4 @@ async function fetchTokenPrices() {
     }
 }
 
-// Fetch liquidity pool data using Uniswap The Graph API
-async function fetchLiquidityData() {
-    const graphAPI = process.env.UNISWAP_API_KEY;
-    try {
-        const response = await axios.post(graphAPI, {
-            query: `
-            {
-                pools(first: 5) {
-                    id
-                    token0 {
-                        symbol
-                    }
-                    token1 {
-                        symbol
-                    }
-                    totalLiquidity
-                }
-            }
-            `,
-        });
-        logger.info("Fetched liquidity pool data successfully.");
-        return response.data.data.pools;
-    } catch (error) {
-        logger.error(`Error fetching liquidity data: ${error.message}`);
-        throw error;
-    }
-}
-
-module.exports = { fetchTokenPrices, fetchLiquidityData };
+module.exports = { fetchTokenPrices };
