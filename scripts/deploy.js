90a406c0a23a6f70ce9785d74adbb3c17e7777a2
techmorebu <techmorebu@gmail.com>
2024-11-20 22:39:27 -0600

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const hre = require("hardhat");
 
 async function main() {
-    const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
-  console.log("Deploying contract...");
-  const simpleStorage = await SimpleStorageFactory.deploy();
-  console.log("Waiting for contract to deployed...");
-  await simpleStorage.deployed();
-  console.log("Contract address");
-  console.log(simpleStorage.address);
+    const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+    const simpleStorage = await SimpleStorage.deploy();
+
+    await simpleStorage.deployed();
+
+    console.log(`SimpleStorage deployed to: ${simpleStorage.address}`);
+}
 
 main()
     .then(() => process.exit(0))
