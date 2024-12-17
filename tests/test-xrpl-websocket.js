c7b6fd2008fda3a28da39c7eac6e918257bdb85b
techmorebu <techmorebu@gmail.com>
2024-12-17 15:30:11 -0600

    Create test-xrpl-websocket.js

---
+++ tests/test-xrpl-websocket.js
@@ -0,0 +1,7 @@
+const { startXrplWebSocket } = require('../data-collection/fetch-xrpl-websocket');
+
+(async () => {
+    console.log('Starting XRPL WebSocket test...');
+    const testAccount = 'rEb8TK3gBgk5auZkwc6sHnwrGVJH8DuaLh'; // Replace with a real XRPL account if needed
+    startXrplWebSocket(testAccount);
+})();
