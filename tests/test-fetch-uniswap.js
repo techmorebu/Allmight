66df07a7a0b653de79d2b11269ef64bf0c8cebf5
techmorebu <techmorebu@gmail.com>
2024-12-23 00:06:16 -0600

    Update test-fetch-uniswap.js

--- tests/test-fetch-uniswap.js
+++ tests/test-fetch-uniswap.js
@@ -1,11 +1,42 @@
-const { fetchUniswapData } = require('../data-collection/fetch-uniswap-data');
+// Required libraries
+const { fetchUniswapData } = require('../fetchers/uniswap-fetcher');
+const winston = require('winston');
 
-(async () => {
+// Logger configuration
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [new winston.transports.Console()]
+});
+
+/**
+ * Test function for Uniswap V3 Fetcher
+ */
+async function testUniswapFetcher() {
+    logger.info('Starting test for Uniswap V3 Fetcher...');
     try {
-        console.log('Testing Uniswap Pool Data...');
-        const pools = await fetchUniswapData();
-        console.log('Fetched Uniswap Pools:', pools);
+        const data = await fetchUniswapData();
+
+        if (data && data.length > 0) {
+            logger.info('Uniswap V3 Fetcher Test Passed', { poolCount: data.length });
+            console.table(data.map(pool => ({
+                ID: pool.id,
+                Token0: pool.token0,
+                Token1: pool.token1,
+                FeeTier: pool.feeTier,
+                Liquidity: pool.liquidity,
+                SqrtPrice: pool.sqrtPrice
+            })));
+        } else {
+            logger.warn('Uniswap V3 Fetcher returned no data');
+        }
     } catch (error) {
-        console.error('Error fetching Uniswap data:', error.message);
+        logger.error('Uniswap V3 Fetcher Test Failed', { error: error.message });
     }
-})();
+}
+
+// Run the test
+testUniswapFetcher();
