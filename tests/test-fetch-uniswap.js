c90ab8ac755798427048f3cde80d255ee341a87f
techmorebu <techmorebu@gmail.com>
2024-12-23 00:47:49 -0600

    Update test-fetch-uniswap.js

--- tests/test-fetch-uniswap.js
+++ tests/test-fetch-uniswap.js
@@ -3,8 +3,8 @@ const { fetchUniswapData } = require('../data-collection/fetch-uniswap-data');
 (async () => {
     try {
         console.log('Testing Uniswap Pool Data...');
-        const pools = await fetchUniswapData();
-        console.log('Fetched Uniswap Pools:', pools);
+        const pools = await  fetchUniswapPairData();
+        console.log('Fetched Uniswap Pair Data cuh..:', pools);
     } catch (error) {
         console.error('Error fetching Uniswap data:', error.message);
     }
