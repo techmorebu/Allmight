42211f841b04688b68d8f91696eb6b00155912d7
techmorebu <techmorebu@gmail.com>
2024-12-24 00:35:59 -0600

    Update test-fetch-dydx-websocket.js

--- tests/test-fetch-dydx-websocket.js
+++ tests/test-fetch-dydx-websocket.js
@@ -1,18 +1,10 @@
 const { connectToDYDX } = require('../data-collection/fetch-dydx-data');
 const { logger } = require('../monitoring/logger');
 
-/**
- * Test dYdX WebSocket Fetcher
- */
-async function testDYDXWebSocketFetcher() {
-    try {
-        logger.info('Starting dYdX WebSocket fetcher test...');
-        connectToDYDX();
-
-        logger.info('Test completed.');
-    } catch (error) {
-        logger.error(`Error during test: ${error.message}`);
-    }
+async function testDYDXFetcher() {
+    logger.info('Starting dYdX WebSocket fetcher test...');
+    connectToDYDX();
+    logger.info('Test completed.');
 }
 
-testDYDXWebSocketFetcher();
+testDYDXFetcher();
