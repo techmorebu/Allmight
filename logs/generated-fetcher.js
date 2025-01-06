d7b3639b556ec32a7958d7180278d62d2fd086b8
techmorebu <techmorebu@gmail.com>
2025-01-05 18:44:45 -0600

    wrd

--- logs/generated-fetcher.js
+++ logs/generated-fetcher.js
@@ -1,18 +1,37 @@
+
 require("dotenv").config();
 const fetch = require("node-fetch");
 
 async function fetchData() {
-  const response = await fetch(process.env.NEW_DEX_API_URL);
+  const response = await fetch(process.env.NEW_DEX_API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `
+      {
+        pools {
+          id
+date
+volumeMatic
+volumeUSD
+volumeUSDUntracked
+feesUSD
+txCount
+tvlUSD
+        }
+      }
+      `,
+    }),
+  });
+
   const data = await response.json();
-
-  const validatedData = data.filter(item => validate(item));
+  const validatedData = data.data.pools.filter((item) => validate(item));
   console.log("Validated Data:", JSON.stringify(validatedData, null, 2));
-
   return validatedData;
 }
 
 function validate(item) {
-  const requiredFields = ["price","volumeUSD","liquidityUSD"];
+  const requiredFields = ["id","date","volumeMatic","volumeUSD","volumeUSDUntracked","feesUSD","txCount","tvlUSD"];
   for (const field of requiredFields) {
     if (!item[field]) return false;
   }
