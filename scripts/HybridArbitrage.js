fd5ae839abb349313c2b9c3a743513c5bea4e8c5
techmorebu <techmorebu@gmail.com>
2025-01-14 02:34:13 -0600

    Update HybridArbitrage.js

--- scripts/HybridArbitrage.js
+++ scripts/HybridArbitrage.js
@@ -4,8 +4,8 @@
 const ethers = require('ethers'); // Explicitly import ethers6 for Hybrid Arbitrage
 const ethers67 = require('ethers67'); // Explicitly import ethers67 for Flashbots
 const { FlashbotsBundleProvider } = require('@flashbots/ethers-provider-bundle');
-const mapper = require("./tools/universal-field-mapper.js"); // Universal Mapper module
-const crossReference = require("./tools/cross-referencing.js"); // Cross-Referencing integration
+const mapper = require("../tools/universal-field-mapper.js"); // Universal Mapper module
+const crossReference = require("../tools/cross-referencing.js"); // Cross-Referencing integration
 require('dotenv').config();
 
 const config = {
