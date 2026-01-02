9e45fbc358b22aacde07e8c03ade5279bfd778dc
techmorebu <techmorebu@gmail.com>
2026-01-01 18:34:12 -0600

    tools: add rapid_scan zero-guess project health probe

---
+++ tools/rapid_scan.py
@@ -0,0 +1,38 @@
+ALLMIGHT RAPID SCAN
+============================================================
+As-of: 2026-01-02 00:33 UTC
+
+1) GIT STATUS
+--------------------
+
+
+2) TEST STATUS (pytest -q)
+--------------------
+........................................................................ [ 92%]
+......                                                                   [100%]
+78 passed in 4.31s
+
+3) CURRENT PHASE
+--------------------
+Latest handoff: docs/phase9/PHASE9_HANDOFF_PROMPT_PHASE10.txt
+
+4) AUTHORITATIVE FILES TO READ
+--------------------
+docs/phase9/PHASE9_HANDOFF_PROMPT_PHASE10.txt
+docs/phase9/PHASE9_ARCHITECTURE_READONLY_LIVE.txt
+docs/phase9/PHASE9_OPERATOR_NOTES.txt
+docs/phase9/PHASE9_TEST_PLAN_NEGATIVE_CASES.txt
+
+5) APPENDICES (MANDATORY)
+--------------------
+docs/appendices/APPX_CHAT_RESPONSE_PROTOCOL.txt
+docs/appendices/APPX_PHASE0_PROTOCOL_PHASE_WRAP_STANDARD.txt
+docs/appendices/APPX_PHASE3_20251224_BUG_NOTES.txt
+docs/appendices/APPX_PHASE3_20251224_BUILD_NOTES.txt
+docs/appendices/APPX_PHASE3_20251224_CHAT_PRESENTATION_PROTOCOL.txt
+docs/appendices/APPX_PHASE3_20251224_PHASE4_HANDOFF.txt
+docs/appendices/APPX_PROJECT_RAPID_SCAN_PROTOCOL.txt
+
+6) META
+--------------------
+If anything above is unclear, the handoff or appendices are incomplete.
